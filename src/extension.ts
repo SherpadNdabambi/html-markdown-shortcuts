3981dfca5bfdacf0368c6bbbfbba4b25a8417cf5
Sherpad Ndabambi <sgndabambi@gmail.com>
2025-05-02 16:52:30 +0200

    Apply Prettier formatting to extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -3,164 +3,176 @@ let originalText: string;
 
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { escape } from 'querystring';
-import * as vscode from 'vscode';
+import { escape } from "querystring";
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log("HTML & Markdown Shortcuts is now active!");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('HTML & Markdown Shortcuts is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let changeCase = vscode.commands.registerCommand(
+    "html-markdown-shortcuts.changeCase",
+    () => {
+      // declare local constants
+      const editor = vscode.window.activeTextEditor,
+        selection = editor?.selection;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let changeCase = vscode.commands.registerCommand("html-markdown-shortcuts.changeCase", () => {
+      if (selection && !selection.isEmpty) {
+        // declare local constants
+        const selectionRange = new vscode.Range(
+            selection.start.line,
+            selection.start.character,
+            selection.end.line,
+            selection.end.character
+          ),
+          selectedText = editor.document.getText(selectionRange);
 
-		// declare local constants
-		const editor = vscode.window.activeTextEditor,
-			selection = editor?.selection;
-	
-		if (selection && !selection.isEmpty) {
+        if (
+          !originalText ||
+          originalText.toLocaleUpperCase() !== selectedText.toLocaleUpperCase()
+        )
+          originalText = selectedText;
 
-			// declare local constants
-			const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character),
-				selectedText = editor.document.getText(selectionRange);
+        if (selectedText === originalText)
+          if (isLowerCase(selectedText))
+            vscode.commands.executeCommand(
+              "editor.action.transformToTitlecase"
+            );
+          else
+            vscode.commands.executeCommand(
+              "editor.action.transformToLowercase"
+            );
+        else if (isLowerCase(selectedText))
+          vscode.commands.executeCommand("editor.action.transformToTitlecase");
+        else if (isTitleCase(selectedText))
+          vscode.commands.executeCommand("editor.action.transformToUppercase");
+        else if (isUpperCase(selectedText))
+          editor.edit((selectedText) => {
+            selectedText.replace(selection, originalText);
+          });
+        else
+          vscode.commands.executeCommand("editor.action.transformToLowercase");
+      }
+    }
+  );
 
-			if (!originalText || originalText.toLocaleUpperCase() !== selectedText.toLocaleUpperCase())
-				originalText = selectedText;
+  let toggleStag = vscode.commands.registerCommand(
+    "html-markdown-shortcuts.toggleStag",
+    () => {
+      // declare local constants
+      const editor = vscode.window.activeTextEditor,
+        selection = editor?.selection;
 
-			if (selectedText === originalText)
-				if (isLowerCase(selectedText))
-					vscode.commands.executeCommand("editor.action.transformToTitlecase");
-				else
-					vscode.commands.executeCommand("editor.action.transformToLowercase");
-			else
-				if (isLowerCase(selectedText))
-					vscode.commands.executeCommand("editor.action.transformToTitlecase");
-				else
-					if (isTitleCase(selectedText))
-						vscode.commands.executeCommand("editor.action.transformToUppercase");
-					else
-						if (isUpperCase(selectedText))
-							editor.edit(selectedText => {
-								selectedText.replace(selection, originalText);
-							});
-						else vscode.commands.executeCommand("editor.action.transformToLowercase");
-		}
-	});
+      if (selection && !selection.isEmpty) {
+        const selectionRange = new vscode.Range(
+            selection.start.line,
+            selection.start.character,
+            selection.end.line,
+            selection.end.character
+          ),
+          selectedText = editor.document.getText(selectionRange);
 
-	let toggleStag = vscode.commands.registerCommand("html-markdown-shortcuts.toggleStag", () => {
+        editor.edit((textEdit) => {
+          if (selectedText.slice(0, 3) === "<s>") {
+            let unstruckText = selectedText
+              .replace(/<\/*s>/, "")
+              .replace(/<\/*s>/, "");
+            textEdit.replace(selection, unstruckText);
+          } else {
+            let struckText = `<s>${selectedText}</s>`;
+            textEdit.replace(selection, struckText);
+          }
+        });
+      }
+    }
+  );
 
-		// declare local constants
-		const editor = vscode.window.activeTextEditor,
-			selection = editor?.selection;
+  let toggleTildeWrap = vscode.commands.registerCommand(
+    "html-markdown-shortcuts.toggleTildeWrap",
+    () => {
+      // declare local constants
+      const editor = vscode.window.activeTextEditor,
+        selection = editor?.selection;
 
-			if (selection && !selection.isEmpty) {
+      if (selection && !selection.isEmpty) {
+        const selectionRange = new vscode.Range(
+            selection.start.line,
+            selection.start.character,
+            selection.end.line,
+            selection.end.character
+          ),
+          selectedText = editor.document.getText(selectionRange);
 
-				const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character),
-					selectedText = editor.document.getText(selectionRange);
+        editor.edit((textEdit) => {
+          if (selectedText.slice(0, 2) === "~~") {
+            let unstruckText = selectedText.replace("~~", "").replace("~~", "");
+            textEdit.replace(selection, unstruckText);
+          } else {
+            let struckText = `~~${selectedText}~~`;
+            textEdit.replace(selection, struckText);
+          }
+        });
+      }
+    }
+  );
 
-				editor.edit(textEdit => {
-
-					if (selectedText.slice(0, 3) === "<s>") {
-
-						let unstruckText = selectedText.replace(/<\/*s>/, '',).replace(/<\/*s>/, '',);
-						textEdit.replace(selection, unstruckText);
-					} else {
-
-						let struckText = `<s>${selectedText}</s>`;
-						textEdit.replace(selection, struckText);
-					}
-				});
-			}
-	});
-
-	let toggleTildeWrap = vscode.commands.registerCommand("html-markdown-shortcuts.toggleTildeWrap", () => {
-
-		// declare local constants
-		const editor = vscode.window.activeTextEditor,
-			selection = editor?.selection;
-
-			if (selection && !selection.isEmpty) {
-
-				const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character),
-					selectedText = editor.document.getText(selectionRange);
-
-				editor.edit(textEdit => {
-
-					if (selectedText.slice(0, 2) === "~~") {
-
-						let unstruckText = selectedText.replace("~~", '',).replace("~~", '',);
-						textEdit.replace(selection, unstruckText);
-					} else {
-
-						let struckText = `~~${selectedText}~~`;
-						textEdit.replace(selection, struckText);
-					}
-				});
-			}
-	});
-
-	context.subscriptions.push(changeCase, toggleStag, toggleTildeWrap);
+  context.subscriptions.push(changeCase, toggleStag, toggleTildeWrap);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
 
 /**
- * 
+ *
  * @param {String} text
  * @returns {Boolean}
  */
-function isLowerCase (text: string) {
+function isLowerCase(text: string) {
+  // declare local variables
+  let result = true;
 
-	// declare local variables
-	let result = true;
-
-	text.split('').forEach(letter => {
-
-		if (typeof(letter) === "string")
-			if (letter !== letter.toLowerCase()) result = false;
-	});
-	return result;
+  text.split("").forEach((letter) => {
+    if (typeof letter === "string")
+      if (letter !== letter.toLowerCase()) result = false;
+  });
+  return result;
 }
 
 /**
- * 
+ *
  * @param {String} sentence
  * @returns {Boolean}
  */
-function isTitleCase (sentence: string) {
+function isTitleCase(sentence: string) {
+  // declare local variables
+  let result = true;
 
-	// declare local variables
-	let result = true;
-
-	if (isUpperCase(sentence)) result = false;
-	sentence.split(' ').forEach(word => {
-		
-		if (typeof(word[0]) === "string")
-			if (word[0] !== word[0].toUpperCase()) result = false;
-	});
-	return result;
+  if (isUpperCase(sentence)) result = false;
+  sentence.split(" ").forEach((word) => {
+    if (typeof word[0] === "string")
+      if (word[0] !== word[0].toUpperCase()) result = false;
+  });
+  return result;
 }
 
 /**
- * 
+ *
  * @param {String} text
  * @returns {Boolean}
  */
-function isUpperCase (text: string) {
+function isUpperCase(text: string) {
+  // declare local variables
+  let result = true;
 
-	// declare local variables
-	let result = true;
-
-	text.split('').forEach(letter => {
-
-		if (typeof(letter) === "string")
-			if (letter !== letter.toUpperCase()) result = false;
-	});
-		return result;
+  text.split("").forEach((letter) => {
+    if (typeof letter === "string")
+      if (letter !== letter.toUpperCase()) result = false;
+  });
+  return result;
 }

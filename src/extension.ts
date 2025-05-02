4330905868d93d4cd2c813a9c9f65ceca7535d64
Sherpad Ndabambi <sgndabambi@gmail.com>
2025-05-02 20:45:21 +0200

    Implement Generate Table of Contents feature

--- src/extension.ts
+++ src/extension.ts
@@ -203,8 +203,8 @@ export function activate(context: vscode.ExtensionContext) {
 
   // Auto-update TOC on save for Markdown files
   context.subscriptions.push(
-    vscode.workspace.onDidSaveTextDocument((document) => {
-      if (document.languageId === "markdown") {
+    vscode.workspace.onWillSaveTextDocument((event) => {
+      if (event.document.languageId === "markdown") {
         vscode.commands.executeCommand("html-markdown-shortcuts.generateToc"); // Reuse the existing generateToc logic
       }
     })
